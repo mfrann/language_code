0df00b86b8e1d35a1a7854bf132a38a1d92f4e2d
Fran <mfranfc@gmail.com>
2025-10-15 10:42:44 -0500

    python polimorfismo

---
+++ python/poo/polimorfismo.py
@@ -0,0 +1,16 @@
+
+
+
+'''
+PILAR DE LA POO JUNTO CON LA HERENCIA
+
+Muchas formas: objetos toman diferentes formas
+
+Ejm.
+Gato: 
+    Salvaje
+        acariciar() -> rabia
+    Domesticado
+        acariciar() -> alegria
+
+'''
