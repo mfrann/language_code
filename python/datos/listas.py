3d7a52f9c6d39e613b29d1be1f239bf0145b3829
Fran <mfranfc@gmail.com>
2025-10-14 21:07:06 -0500

    python lists update

--- python/datos/listas.py
+++ python/datos/listas.py
@@ -23,14 +23,23 @@ print(f'Los numeros ganadores son: {number}')
 '''
 
 #METODOS PARA ELIMINAR
-
+''''
 list = [1, 34, 54, 'Hola']
 
 for i in list:
     print(i)
 
 #list.remove(1) #valor
-list.pop(2) #posicion
+#list.pop(2) #posicion
+#list.clear()
+'''
 
-for i in list:
-    print(i)
+#SI SE REPITE
+list = [1,1,3,5,5]
+print(list.count(1)) #contar cuanto se repite
+
+print(list.index(3)) #en que ligar se encuentra
+
+list.reverse() #reversa
+
+print(list)
